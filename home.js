6fadddbd5ef6149a10a074db45ae82b0f79cbd6b
RMartinic <roko.martinic2@gmail.com>
2024-12-30 21:53:03 +0100

    Work on js and translation

--- home.js
+++ home.js
@@ -1,15 +1,19 @@
-var introtranslation
-var daystransaltion
-fetch("./home.json")
-.then(res =>res.json())
-.then(data=>{
-    introtranslation=data;
-})
-fetch("./working-time.json")
+var introtranslation;
+var daystransaltion;
+var contactTranslation;
+var recommedationTranslation;
+fetch("./mainpage.json")
 .then(response=>response.json())
 .then(data=>{
-    daystransaltion=data;
+    introtranslation=data[0];
+    daystransaltion=data[1];
+    contactTranslation=data[2];
+    recommedationTranslation=data[3];
+    console.log(introtranslation,daystransaltion,contactTranslation,recommedationTranslation);
 })
+.catch(error => {
+  console.error("Failed to fetch JSON data:", error);
+});
 
 
 function chglang(data){
@@ -18,23 +22,26 @@ function chglang(data){
     intro.innerText=introtranslation[sel];
     var days=document.getElementById("working-days");
     days.innerText=daystransaltion[sel];
+    var contactMessage=document.getElementById("contact-message");
+    contactMessage.innerText=contactTranslation[sel];
+    var recommedationText=document.getElementById("recommend-text");
+    recommedationText.innerText=recommedationTranslation[sel];
     }
-    let slideIndex = 1;
-    showSlides(slideIndex);function plusSlide(n) {
-      showSlides(slideIndex += n);
-    }function showSlides(n) {
-      let i;
-      const slides = document.getElementsByClassName("slide");
-      if (n > slides.length) {slideIndex = 1}
-      if (n < 1) {slideIndex = slides.length}
-      for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-      }
-      slides[slideIndex-1].style.display = "flex";
-      slides[slideIndex-1].style.justifyContent="center";
-      slides[slideIndex-1].style.alignItems="center";
+let slideIndex = 1;
+showSlides(slideIndex);function plusSlide(n) {
+  showSlides(slideIndex += n);
+}function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+    }
+  slides[slideIndex-1].style.display = "flex";
+  slides[slideIndex-1].style.justifyContent="center";
+  slides[slideIndex-1].style.alignItems="center";
 
-        
     }function prevSlide() {
       plusSlide(-1);
     }function nextSlide() {
