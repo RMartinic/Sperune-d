ef10982785045b9e75e452c82e259877cbb18ef3
RMartinic <roko.martinic2@gmail.com>
2025-01-21 21:34:47 +0100

    Gallery page added

---
+++ js/gallery.js
@@ -0,0 +1,15 @@
+const listOfImages=document.querySelectorAll('.image');
+listOfImages.forEach(image=>{
+    image.addEventListener("click",toggleImage);
+});
+function toggleImage(){
+    this.classList.add('clicked');
+    const background=document.createElement("div");
+    background.classList.add('blur');
+    const galleryDiv=document.querySelector(".gallery-container");
+    galleryDiv.appendChild(background);
+    background.addEventListener("click",()=>{
+        background.remove();
+        this.classList.remove('clicked');
+    })
+}
