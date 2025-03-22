64e384a50ebf60de3bdf85ec78d00b49843d7978
RMarti <roko.martinic2@gmail.com>
2025-03-23 00:06:53 +0100

    Added js and json to news

---
+++ js/news.js
@@ -0,0 +1,51 @@
+function getSelectedLanguage(){
+    const localStorageLanguage=localStorage.getItem("selectedLanguage");
+    if(localStorageLanguage){
+        document.getElementById("langselect").value=localStorageLanguage;
+        return localStorageLanguage;
+    }
+    else return "eng";
+}
+
+async function getTranslations(){
+    try {
+        const response = await fetch("./json/news.json");
+        
+        if (!response.ok){
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json()
+        for(let index=0;index<data.length;index++){
+            loadNews(data[index],getSelectedLanguage());
+        }
+    }
+    catch(error){
+        console.error("Error fetching news");
+    }
+}
+
+function chglang(){
+    const selectedLanguage=document.getElementById("langselect").value
+    localStorage.setItem("selectedLanguage", selectedLanguage);
+    location.reload();
+}
+
+function loadNews(newsData, selectedLanguage){
+    const targetedContainer=document.getElementById("news-container");
+    const newNews=document.createElement("div");
+    newNews.classList.add("news-article");
+    newNews.innerHTML=`
+    <p class="article-headline">${newsData.title[selectedLanguage]}</p>
+    <div class="horizontal-line"></div>
+    <p class="article-paragraph">${newsData.body[selectedLanguage]}</p>
+    ${newsData.photo?
+        `<img src=${newsData.photo} class=news-picture />`
+        :
+        `<img src="./photos/news-sample.jpg" class=news-picture />`
+    }
+    <div class="vertical-line"></div>
+    <p class="date">${newsData.date}</p>
+    `
+    targetedContainer.appendChild(newNews);
+}
+getTranslations();
